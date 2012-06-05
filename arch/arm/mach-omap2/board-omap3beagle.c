19b38630c2f0b05f78e1f6cc012b60cd115b6c7b
Chris <chris.ladden@liquidware.com>
2012-06-05 11:45:50 -0400

    Machine configuration with amber display, touch, battery, usb adb, usb mass storage, glow leds, gpio switches

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -18,6 +18,7 @@
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/clk.h>
+#include <linux/backlight.h>
 #include <linux/io.h>
 #include <linux/leds.h>
 #include <linux/gpio.h>
@@ -33,6 +34,9 @@
 
 #include <linux/regulator/machine.h>
 #include <linux/i2c/twl.h>
+#include <linux/i2c.h>
+#include <linux/i2c/liquidware_pcap.h>
+#include <linux/i2c/tsc2007.h>
 
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
@@ -46,6 +50,7 @@
 #include <plat/gpmc.h>
 #include <plat/nand.h>
 #include <plat/usb.h>
+#include <plat/pwm.h>
 
 #include "mux.h"
 #include "hsmmc.h"
@@ -60,32 +65,59 @@
 #define GOOGLE_PRODUCT_ID		0x9018
 #define GOOGLE_ADB_PRODUCT_ID		0x9015
 
-static char *usb_functions_adb[] = {
-	"adb",
+static char *usb_functions_ums[] = {
+    "usb_mass_storage",
+};
+
+static char *usb_functions_ums_adb[] = {
+   "usb_mass_storage",
+   "adb",
 };
 
 static char *usb_functions_all[] = {
+    "usb_mass_storage",
 	"adb",
 };
 
 static struct android_usb_product usb_products[] = {
 	{
-		.product_id	= GOOGLE_PRODUCT_ID,
-		.num_functions	= ARRAY_SIZE(usb_functions_adb),
-		.functions	= usb_functions_adb,
+        .product_id     = 0x4e11,
+        .num_functions  = ARRAY_SIZE(usb_functions_ums),
+        .functions      = usb_functions_ums,
 	},
+	{
+       .product_id     = 0x4e12,
+       .num_functions  = ARRAY_SIZE(usb_functions_ums_adb),
+       .functions      = usb_functions_ums_adb,
+    },
 };
 
+static struct usb_mass_storage_platform_data mass_storage_pdata = {
+       .nluns          = 1,
+       .vendor         = "Liquidware",
+       .product        = "Liquidware Android",
+       .release        = 0x0100,
+};
+
+static struct platform_device usb_mass_storage_device = {
+       .name   = "usb_mass_storage",
+       .id     = -1,
+       .dev    = {
+               .platform_data = &mass_storage_pdata,
+        },
+ };
+
 static struct android_usb_platform_data android_usb_pdata = {
-	.vendor_id	= GOOGLE_VENDOR_ID,
-	.product_id	= GOOGLE_PRODUCT_ID,
-	.functions	= usb_functions_all,
-	.products	= usb_products,
+    .vendor_id      = 0x18d1,
+    .product_id     = 0x4e12,
 	.version	= 0x0100,
-	.product_name	= "rowboat gadget",
-	.manufacturer_name	= "rowboat",
+	.product_name	= "Liquidware Android",
+	.manufacturer_name	= "Liquidware",
 	.serial_number	= "20100720",
-	.num_functions	= ARRAY_SIZE(usb_functions_all),
+    .num_products = ARRAY_SIZE(usb_products),
+    .products = usb_products,
+    .num_functions = ARRAY_SIZE(usb_functions_all),
+    .functions = usb_functions_all,
 };
 
 static struct platform_device androidusb_device = {
@@ -98,6 +130,7 @@ static struct platform_device androidusb_device = {
 
 static void omap3evm_android_gadget_init(void)
 {
+    platform_device_register(&usb_mass_storage_device);
 	platform_device_register(&androidusb_device);
 }
 #endif
@@ -228,6 +261,17 @@ static struct mtd_partition omap3beagle_nand_partitions[] = {
 
 static int beagle_enable_dvi(struct omap_dss_device *dssdev)
 {
+#define ENABLE_VAUX1_DEDICATED	0x04
+#define ENABLE_VAUX1_DEV_GRP	0x20
+
+    /* Enable VAUX1 regulator */
+    twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
+			ENABLE_VAUX1_DEDICATED,
+			TWL4030_VAUX1_DEDICATED);
+    twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
+			ENABLE_VAUX1_DEV_GRP,
+			TWL4030_VAUX1_DEV_GRP);
+
 	if (gpio_is_valid(dssdev->reset_gpio))
 		gpio_set_value(dssdev->reset_gpio, 1);
 
@@ -243,7 +287,7 @@ static void beagle_disable_dvi(struct omap_dss_device *dssdev)
 static struct omap_dss_device beagle_dvi_device = {
 	.type = OMAP_DISPLAY_TYPE_DPI,
 	.name = "dvi",
-	.driver_name = "generic_panel",
+	.driver_name = "liquidware_beagletouch",
 	.phy.dpi.data_lines = 24,
 	.reset_gpio = -EINVAL,
 	.platform_enable = beagle_enable_dvi,
@@ -282,6 +326,19 @@ static struct regulator_consumer_supply beagle_vdac_supply =
 static struct regulator_consumer_supply beagle_vdvi_supply =
 	REGULATOR_SUPPLY("vdds_dsi", "omapdss");
 
+/* Pins for the BeagleTouch */
+#define CS_PIN			139
+#define MISO_PIN		146
+#define MOSI_PIN		144
+#define CLK_PIN			138
+//#define RESET_PIN		137 //oled
+#define RESET_PIN		129 //openpad
+#define PANEL_PWR_PIN	143
+
+#define OMAP3_BEAGLETOUCH_TS_INT 	136 //BeagleTouch
+//#define OMAP3_BEAGLETOUCH_TS_INT 	15  //OpenPad
+#define OMAP3_BEAGLETOUCH_TS_RESET	158 //BeagleTouch
+
 static void __init beagle_display_init(void)
 {
 	int r;
@@ -293,6 +350,39 @@ static void __init beagle_display_init(void)
 	}
 
 	gpio_direction_output(beagle_dvi_device.reset_gpio, 0);
+
+        /* BeagleTouch GPIO */
+	r |= omap_mux_init_gpio(CS_PIN, OMAP_PIN_OUTPUT);
+	r |= omap_mux_init_gpio(MISO_PIN, OMAP_PIN_INPUT_PULLUP);
+	r |= omap_mux_init_gpio(MOSI_PIN, OMAP_PIN_OUTPUT);
+	r |= omap_mux_init_gpio(CLK_PIN, OMAP_PIN_OUTPUT);
+	r |= omap_mux_init_gpio(RESET_PIN, OMAP_PIN_OUTPUT);
+	r |= omap_mux_init_gpio(PANEL_PWR_PIN, OMAP_PIN_OUTPUT);
+
+	/* OpenPad switches */
+	r |= omap_mux_init_gpio(152, OMAP_PIN_INPUT_PULLUP);
+	r |= omap_mux_init_gpio(153, OMAP_PIN_INPUT_PULLUP);
+	r |= omap_mux_init_gpio(154, OMAP_PIN_INPUT_PULLUP);
+	r |= omap_mux_init_gpio(155, OMAP_PIN_INPUT_PULLUP);
+
+	if (r < 0) {
+		printk(KERN_ERR "Unable to init display GPIO\n");
+		return;
+	}
+
+	/* OpenPad Backlight PWM */
+	r = omap_mux_init_signal("gpmc_ncs4.gpt9_pwm_evt", OMAP_PIN_OUTPUT);
+	if (r < 0) {
+		printk(KERN_ERR "Unable to init display backlight GPIO\n");
+		return;
+	}
+
+	/* BeagleTouch Backlight PWM */
+	r = omap_mux_init_signal("uart2_cts.gpt9_pwm_evt", OMAP_PIN_OUTPUT);
+	if (r < 0) {
+		printk(KERN_ERR "Unable to init display backlight GPIO\n");
+		return;
+	}
 }
 
 #include "sdram-micron-mt46h32m32lf-6.h"
@@ -364,7 +454,8 @@ static int beagle_twl_gpio_setup(struct device *dev,
 		gpio_direction_output(gpio + TWL4030_GPIO_MAX, 0);
 
 	/* DVI reset GPIO is different between beagle revisions */
-	if (omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XM || omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XMC)
+	if (omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XM ||
+		omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XMC)
 		beagle_dvi_device.reset_gpio = 129;
 	else
 		beagle_dvi_device.reset_gpio = 170;
@@ -403,6 +494,77 @@ static int beagle_twl_gpio_setup(struct device *dev,
 
 	return 0;
 }
+
+static int liquidware_pcap_get_pendown_state(void)
+{
+	return !gpio_get_value(OMAP3_BEAGLETOUCH_TS_INT);
+}
+
+static void liquidware_pcap_perform_reset(void)
+{
+	gpio_direction_output(OMAP3_BEAGLETOUCH_TS_RESET, 1);
+	mdelay(10);
+	gpio_direction_output(OMAP3_BEAGLETOUCH_TS_RESET, 0);
+
+}
+
+static int omap3beagletouch_liquidware_pcap_init(struct liquidware_pcap_platform_data *pdata)
+{
+	int gpio;
+	int ret = 0;
+
+	gpio = pdata->pen_gpio;
+	ret |= omap_mux_init_gpio(gpio, OMAP_PIN_INPUT);
+	ret |= gpio_request(gpio, "liquidware_pcap ts_int");
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to request GPIO %d for "
+				"liquidware_pcap\n", gpio);
+		return ret;
+	}
+	gpio_direction_input(gpio);
+
+	gpio = pdata->reset_gpio;
+	ret |= omap_mux_init_gpio(gpio, OMAP_PIN_OUTPUT);
+	ret |= gpio_request(gpio, "liquidware_pcap reset");
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to request GPIO %d for "
+				"liquidware_pcap\n", gpio);
+		return ret;
+	}
+	gpio_direction_output(gpio, 0);
+
+	return ret;
+}
+
+static struct liquidware_pcap_platform_data liquidware_pcap_info = {
+	.model			= 2007,
+	.x_plate_ohms	= 180,
+	.pen_gpio		= OMAP3_BEAGLETOUCH_TS_INT,
+	.reset_gpio		= OMAP3_BEAGLETOUCH_TS_RESET,
+	.get_pendown_state	= liquidware_pcap_get_pendown_state,
+	.perform_reset		= liquidware_pcap_perform_reset,
+	.init_platform_hw	= omap3beagletouch_liquidware_pcap_init,
+};
+
+static struct tsc2007_platform_data tsc2007_info = {
+	.model				= 2007,
+	.x_plate_ohms		= 180,
+    .get_pendown_state  = NULL,
+};
+
+static struct i2c_board_info __initdata i2c3_clients[] = {
+	{
+		I2C_BOARD_INFO("liquidware_pcap", 0x38),
+		.irq	= OMAP_GPIO_IRQ(OMAP3_BEAGLETOUCH_TS_INT),
+		.platform_data	= &liquidware_pcap_info,
+	},
+#if 0
+	{
+		I2C_BOARD_INFO("tsc2007", 0x48),
+		.platform_data	= &tsc2007_info,
+	},
+#endif
+};
 
 static struct twl4030_gpio_platform_data beagle_gpio_data = {
 	.gpio_base	= OMAP_MAX_GPIO_LINES,
@@ -548,20 +710,39 @@ static struct i2c_board_info __initdata beagle_i2c_eeprom[] = {
        },
 };
 
+static struct i2c_board_info __initdata beagle_i2c2_boardinfo[] = {
+	{
+		I2C_BOARD_INFO("bq27200_battery", 0x0B),
+	},
+};
+
 static int __init omap3_beagle_i2c_init(void)
 {
 	omap_register_i2c_bus(1, 2600, beagle_i2c_boardinfo,
 			ARRAY_SIZE(beagle_i2c_boardinfo));
 
 	/* Bus 2 is used for Camera/Sensor interface */
-	omap_register_i2c_bus(2, 400, NULL, 0);
+	omap_register_i2c_bus(2, 50, beagle_i2c2_boardinfo, ARRAY_SIZE(beagle_i2c2_boardinfo));
 
 	/* Bus 3 is attached to the DVI port where devices like the pico DLP
 	 * projector don't work reliably with 400kHz */
-	omap_register_i2c_bus(3, 100, beagle_i2c_eeprom, ARRAY_SIZE(beagle_i2c_eeprom));
+	omap_register_i2c_bus(3, 100,  i2c3_clients, ARRAY_SIZE(i2c3_clients));
 
 	return 0;
 }
+
+static struct omap2_pwm_platform_config pwm_config = {
+	.timer_id           = 9,   // GPT9_PWM_EVT
+	.polarity           = 0    // Active-high
+};
+
+static struct platform_device pwm_device = {
+	.name               = "omap-pwm",
+	.id                 = 0,
+	.dev                = {
+		.platform_data  = &pwm_config
+	}
+};
 
 static struct gpio_led gpio_leds[] = {
 	{
@@ -573,6 +754,42 @@ static struct gpio_led gpio_leds[] = {
 		.name			= "beagleboard::usr1",
 		.default_trigger	= "mmc0",
 		.gpio			= 149,
+	},
+	{
+		.name			= "openpad::glow-t",
+		.gpio			= 34,
+	},
+	{
+		.name			= "openpad::glow0",
+		.gpio			= 50,
+	},
+        {
+		.name			= "openpad::glow1",
+		.gpio			= 51,
+	},
+	{
+		.name			= "openpad::glow2",
+		.gpio			= 49,
+	},
+        {
+		.name			= "openpad::glow3",
+		.gpio			= 47,
+	},
+	{
+		.name			= "openpad::glow4",
+		.gpio			= 45,
+	},
+        {
+		.name			= "openpad::glow5",
+		.gpio			= 46,
+	},
+	{
+		.name			= "openpad::glow6",
+		.gpio			= 48,
+	},
+        {
+		.name			= "openpad::glow7",
+		.gpio			= 44,
 	},
 	{
 		.name			= "beagleboard::pmu_stat",
@@ -596,10 +813,38 @@ static struct platform_device leds_gpio = {
 
 static struct gpio_keys_button gpio_buttons[] = {
 	{
-		.code			= BTN_EXTRA,
-		.gpio			= 7,
-		.desc			= "user",
+		.code			= 125,
+		.gpio			= 4,
+		.desc			= "POWER",
 		.wakeup			= 1,
+	},
+	{
+		.code			= 102,
+		.gpio			= 152,
+		.desc			= "HOME",
+		.wakeup			= 1,
+		.active_low 	= 1,
+	},
+	{
+		.code			= 109,
+		.gpio			= 153,
+		.desc			= "VOL_DOWN",
+		.wakeup			= 1,
+		.active_low 	= 1,
+	},
+	{
+		.code			= 104,
+		.gpio			= 154,
+		.desc			= "VOL_UP",
+		.wakeup			= 1,
+		.active_low 	= 1,
+	},
+	{
+		.code			= 1,
+		.gpio			= 155,
+		.desc			= "BACK",
+		.wakeup			= 1,
+		.active_low 	= 1,
 	},
 };
 
@@ -635,6 +880,7 @@ static struct platform_device *omap3_beagle_devices[] __initdata = {
 	&leds_gpio,
 	&keys_gpio,
 	&beagle_dss_device,
+        &pwm_device,
 };
 
 static void __init omap3beagle_flash_init(void)
@@ -683,6 +929,44 @@ static const struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
 
 #ifdef CONFIG_OMAP_MUX
 static struct omap_board_mux board_mux[] __initdata = {
+
+	/* LCD PWM */
+	OMAP3_MUX(GPMC_NCS4, OMAP_MUX_MODE3 | OMAP_PIN_OUTPUT), //OpenPad
+	OMAP3_MUX(UART2_CTS, OMAP_MUX_MODE2 | OMAP_PIN_OUTPUT), //BeagleTouch
+
+	/* Touchscreen */
+	//OMAP3_MUX(ETK_D1, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP), //OpenPad Touch INT
+	OMAP3_MUX(MCBSP1_DX, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT), //OpenPad Touch RESET
+	//OMAP3_MUX(SDMMC2_DAT4, OMAP_MUX_MODE3 | OMAP_PIN_INPUT_PULLUP), //BeagleTouch Touch INT
+#if 0
+	/* Liquidware BeagleTouch */
+	OMAP3_MUX(SDMMC2_DAT7, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 139
+	OMAP3_MUX(UART2_TX,    OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP), //GPIO 146
+	OMAP3_MUX(UART2_CTS,   OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 144
+	OMAP3_MUX(SDMMC2_DAT6, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 138
+	OMAP3_MUX(SDMMC2_DAT5, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 137
+	OMAP3_MUX(SDMMC1_DAT7, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 129
+	OMAP3_MUX(MCBSP3_FSX,  OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),       //GPIO 143
+#endif
+
+	/* Liquidware OpenPad Glow LEDs */
+	OMAP3_MUX(GPMC_A1, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D15, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D14, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D13, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D12, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D11, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D10, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D9, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+	OMAP3_MUX(GPMC_D8, OMAP_MUX_MODE4 | OMAP_PIN_OUTPUT),
+
+	/* Liquidware OpenPad Keys */
+	OMAP3_MUX(SYS_BOOT2, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLDOWN),			//GPIO 4
+	OMAP3_MUX(MCBSP4_CLKX, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP),			//GPIO 152
+	OMAP3_MUX(MCBSP4_DR, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP),			//GPIO 153
+	OMAP3_MUX(MCBSP4_DX, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP),			//GPIO 154
+	OMAP3_MUX(MCBSP4_FSX, OMAP_MUX_MODE4 | OMAP_PIN_INPUT_PULLUP),			//GPIO 155
+
 	{ .reg_offset = OMAP_MUX_TERMINATOR },
 };
 #endif
